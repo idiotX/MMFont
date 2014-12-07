973d453cf5eb17d7d288d37f214885b2001671fc
nyinyitun83 <nyinyitun83@gmail.com>
2014-12-08 01:32:34 +0800

    Update mmf.js

--- js/mmf.js
+++ js/mmf.js
@@ -1756,7 +1756,7 @@ function applyUnicodeFont(doc) {
 ﻿  ﻿  var fileref = doc.createElement('link');
 ﻿  ﻿  fileref.setAttribute('rel', 'stylesheet');
 ﻿  ﻿  fileref.setAttribute('type', 'text/css');
-﻿  ﻿  fileref.setAttribute('href', 'https://raw.githubusercontent.com/nyinyitun83/MMFont/master/fonts/cherry.css');
+﻿  ﻿  fileref.setAttribute('href', 'https://rawgit.com/nyinyitun83/MMFont/master/fonts/cherry.css');
 ﻿  ﻿  head.appendChild(fileref);
 ﻿  }
 ﻿  
